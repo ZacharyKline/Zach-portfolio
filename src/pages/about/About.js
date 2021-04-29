eb61a9e503b0e04342b91767621fcf38d9386150
Zachary Kline <zkline888@gmail.com>
2021-04-29 12:15:01 -0400

    Added some icons of stuff I've done

---
+++ src/pages/about/About.js
@@ -0,0 +1,11 @@
+
+
+function About(props) {
+    return (
+        <div class="about">
+            <h1>About Me:</h1>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/contact/Contact.js
@@ -0,0 +1,8 @@
+
+function Contact(props) {
+    return (
+        <div class='contact'>
+            <h1>Contact Me:</h1>
+        </div>
+    )
+}
