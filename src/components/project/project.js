50a460ed0a882dcb7c30eb07bbf1c67c798da4e5
Zachary Kline <zkline888@gmail.com>
2021-04-30 16:07:13 -0400

    Finished the first round of the projects page. Fairly proud of it

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -1,13 +1,42 @@
+import { Card, Button, Anchor } from "antd";
+import './project.css'
+const { Meta } = Card;
+// const { Link } = Anchor;
 
 
 function Project(props) {
-    return (
-        <div className='singleProject'>
-            <h2>{props.title}</h2>
-            <img src={props.picture} />
-            <p>{props.description}</p>
-        </div>
-    )
+    if (props.git === null ) {
+        return (
+					<div className="projectsSpot">
+						<div className="singleProject">
+							<img src={props.picture} alt={props.alt} />
+
+							<h1>
+								
+									{props.title}
+							
+							</h1>
+
+							<p>{props.description}</p>
+						</div>
+					</div>
+				);
+    }
+	return (
+		<div className="projectsSpot">
+			<div className="singleProject">
+				<img src={props.picture} alt={props.alt} />
+
+				<h1>
+					<a target="_blank" href={props.git}>
+						{props.title}
+					</a>
+				</h1>
+
+				<p>{props.description}</p>
+			</div>
+		</div>
+	);
 }
 
-export default Project
+export default Project;
